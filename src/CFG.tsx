faf18a9646de821e0dbe9404718221838c9faaf3
FDSoftware <dfrenoux@gmail.com>
2020-01-05 22:44:45 -0300

    test view

---
+++ src/CFG.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './css/cfg.scss';
+class CFG extends React.Component{
+  
+    render() {
+        return (
+                <div id="container-2">
+
+                    <div id ="menu-cfg">
+                        <div id ="menu-cfg-item"> Inyeccion</div>
+                        <div id ="menu-cfg-item"> Encendido</div>
+                    </div>
+                    
+                    <div id="cards-cfg-max">
+                    <p>OLA K ACE</p>
+                    </div>
+                </div>
+        );
+    };
+}
+
+/* const App: React.FC = () => {
+
+} */
+export default CFG;
