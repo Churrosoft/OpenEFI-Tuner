f7205d7b7f182c4f77d7c4b6b74a5eef22f3ab65
FDSoftware <dfrenoux@gmail.com>
2020-03-25 18:22:13 -0300

    WIP: (getRPM)

---
+++ src/components/GetRPM.vue
@@ -0,0 +1,32 @@
+<template>
+	<v-container>
+		<v-row class="text-center">
+			<v-col cols="4">
+				<v-btn color="primary" @click="reqTEMP">Get TEMP</v-btn>
+                <v-btn color="primary" @click="reqRPM">Get RPM</v-btn>
+			</v-col>
+		</v-row>
+	</v-container>
+</template>
+
+<script>
+export default {
+    data: () => ({}),
+    methods:{
+        reqTEMP(){
+            if(this.$store.state.connected){
+                this.$store.dispatch('sendMessage',{ command: 2, subcommand: 1 });
+            }else{
+                console.warn("todavia no se puede pelotudo");
+            }
+        },
+        reqRPM(){
+            if(this.$store.state.connected){
+                this.$store.dispatch('sendMessage',{ command: 2, subcommand: 2 });
+            }else{
+                console.warn("todavia no se puede pelotudo");
+            }
+        }
+    }
+}
+</script>
