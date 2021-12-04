6c743e999904dfbe948bcd7cd46b4d01b259b527
FDSoftware <dfrenoux@gmail.com>
2021-12-04 02:15:18 -0300

    quasar goes brrr rrrr rrr

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,10 @@
 <template>
-	<v-app>
-		<v-container>
-			<v-row>
-				<v-col cols="12">
-					<Header />
-				</v-col>
-			</v-row>
-
-			<v-row>
-				<v-col cols="12">
-					<Menu />
-				</v-col>
-			</v-row>
-		</v-container>
-		<v-content>
-			<router-view />
-		</v-content>
-		<v-footer app>
-			<span><a href="https://github.com/openefi/" target="new"> &copy; OpenEFI 2020</a> </span>
-		</v-footer>
-	</v-app>
+  <router-view />
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue';
 
-<script>
-import Header from '@/components/Header.vue';
-import Menu from '@/components/Menu.vue';
-export default {
-	name: 'App',
-
-	components: { Header, Menu },
-
-	data: () => ({
-		//
-	}),
-};
+export default defineComponent({
+  name: 'App'
+})
 </script>
