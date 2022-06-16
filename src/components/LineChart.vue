4e7bd067925d398ebb7bb9064df4aba436d3a0eb
FDSoftware <dfrenoux@gmail.com>
2022-06-16 00:58:03 -0300

    linechart add custom x/y scale

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,13 +1,6 @@
-<template>
-  <div ref="resizeRef" id="svgRef">
-    <svg ref="svgRef">
-      <g class="x-axis" />
-      <g class="y-axis" />
-    </svg>
-  </div>
-</template>
+<script lang="ts" setup>
+/* eslint-disable @typescript-eslint/no-unused-vars */
 
-<script lang="ts">
 import { onMounted, ref } from 'vue';
 import {
   select,
@@ -18,117 +11,159 @@ import {
   drag,
   extent,
   NumberValue,
-  timeParse,
-  scaleTime,
-  csv,
+  max,
+  EnterElement,
 } from 'd3';
 
-export default {
-  name: 'ResponsiveLineChart',
-  props: ['data'],
-  setup(props: any) {
-    const svgRef = ref<Element | null>(null);
+interface LineChartProps {
+  size?: { width: number; height: number };
+  dataKeyA: string;
+  dataKeyB: string;
+  onUpdate?: () => void;
+  data: Array<{ [key: string]: number }>;
+  margins?: {
+    x: Array<number>;
+    y: Array<number>;
+  };
+}
 
-    onMounted(() => {
-      const margin = { top: 10, right: 30, bottom: 30, left: 60 },
-        width = 460 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+type LineChartData = LineChartProps['data'] & { LineChartIndex: number };
 
-      const svg = select(svgRef.value)
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-        .append('g')
-        .attr('transform', `translate(${margin.left},${margin.top})`);
+const svgRef = ref<Element | null>(null);
+const props = defineProps<LineChartProps>();
 
-      let i = 0;
+onMounted(() => {
+  const { dataKeyA, dataKeyB, size, data } = props;
 
-      void csv(
-        'https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/connectedscatter.csv',
+  const margin = { top: 10, right: 30, bottom: 30, left: 60 },
+    width = size?.width ?? 460 - margin.left - margin.right,
+    height = size?.height ?? 400 - margin.top - margin.bottom;
 
-        function (d) {
-          i++;
-          return {
-            date: timeParse('%Y-%m-%d')(d.date as string) ?? new Date(),
-            value: d.value ?? '0',
-            ref: i - 1,
-          };
-        }
-      ).then(function (data) {
-        const customX = scaleTime()
-          .domain(extent(data, (d) => d.date) as Array<Date>)
-          .range([0, width]);
+  const svg = select(svgRef.value)
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
 
-        svg.append('g').attr('transform', `translate(0, ${height})`).call(axisBottom(customX));
+  let dataWithKey = data.map((d, i) => {
+    return { ...d, LineChartIndex: i } as LineChartData;
+  });
 
-        const customY = scaleLinear().domain([8000, 9200]).range([height, 0]);
-
-        svg.append('g').call(axisLeft(customY));
-
-        customX({ date: new Date(), value: '12' }.date);
-
-        interface FuckD3 {
-          date: Date;
-          value: string;
-        }
-
-        const myLine = line()
-          .x((d) => customX((d as unknown as FuckD3).date))
-          .y((d) => customY((d as unknown as FuckD3).value as unknown as NumberValue));
-
-        interface ICordinate {
-          x: number;
-          y: number;
-        }
-
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        function dragstarted(this: any) {
-          select(this).raise().attr('stroke', 'white');
-          select(this).raise().attr('fill', 'red');
-        }
-
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        function dragged(this: any, event: unknown, d: unknown) {
-          select(this)
-            .attr('cx', ((d as ICordinate).x = (event as ICordinate).x))
-            .attr('cy', ((d as ICordinate).y = (event as ICordinate).y));
-        }
-
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        function dragended(this: any, _event: unknown, d: any) {
-          select(this).attr('stroke', null);
-          select(this).raise().attr('fill', '#69b3a2');
-          console.log((d as ICordinate).x, (d as ICordinate).y, d);
-        }
-
-        let dragHandler = drag().on('start', dragstarted).on('drag', dragged).on('end', dragended);
-
-        // Add the points
-        svg
-          .append('g')
-          .selectAll('dot')
-          .data(data)
-          .join('circle')
-          .attr('cx', (d) => customX(d.date))
-          .attr('cy', (d) => customY(d.value as unknown as NumberValue))
-          .attr('r', 5)
-          .attr('fill', '#69b3a2')
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          .call(dragHandler as any);
-
-        svg
-          .append('path')
-          .datum(data)
-          .attr('fill', 'none')
-          .attr('stroke', '#69b3a2')
-          .attr('stroke-width', 1.5)
-          .attr('d', myLine as unknown as string);
-
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        // svg.selectAll('path').call(dragHanlder as any);
-      });
+  const updateData = (key: number, x: number, y: number) => {
+    dataWithKey = dataWithKey.map((el) => {
+      if (el.LineChartIndex === key) {
+        el[dataKeyA as unknown as number] = x as unknown as { [key: string]: number };
+        el[dataKeyB as unknown as number] = y as unknown as { [key: string]: number };
+      }
+      return el;
     });
+  };
 
-    return { svgRef };
-  },
-};
+  extent(dataWithKey, (d) => d['d' as unknown as number] as unknown as number);
+
+  // TODO: prop for max/min/extent
+  const customX = scaleLinear()
+    .domain(
+      props.margins?.x ?? [
+        0,
+        max(dataWithKey, (d) => d[dataKeyA as unknown as number] as unknown as number),
+      ]
+    )
+    .range([0, width]);
+
+  svg.append('g').attr('transform', `translate(0, ${height})`).call(axisBottom(customX));
+
+  const customY = scaleLinear()
+    .domain(
+      props.margins?.y ?? [
+        max(dataWithKey, (d) => d[dataKeyB as unknown as number] as unknown as number),
+        0,
+      ]
+    )
+    .range([0, height]);
+
+  /*   const customYLine = scaleLinear()
+    .domain(
+      extent(
+        dataWithKey,
+        (d) => d[dataKeyB as unknown as number] as unknown as number
+      ) as Iterable<NumberValue>
+    )
+    .range([height, 0]);
+ */
+  svg.append('g').call(axisLeft(customY));
+
+  const myLine = line()
+    .x((d) => customX(d[dataKeyA as unknown as number]))
+    .y((d) => customY(d[dataKeyB as unknown as number]));
+
+  interface ICordinate {
+    x: number;
+    y: number;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function dragstarted(this: any) {
+    select(this).raise().attr('stroke', 'white');
+    select(this).raise().attr('fill', 'red');
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function dragged(this: any, event: unknown, d: unknown) {
+    const ld = d as LineChartData & ICordinate;
+    const levent = event as ICordinate;
+
+    select(this)
+      .attr('cx', (ld.x = levent.x))
+      .attr('cy', (ld.y = levent.y));
+
+    const newX = customX.invert(levent.x);
+    const newY = customY.invert(levent.y);
+
+    updateData(ld.LineChartIndex, newX, newY);
+
+    svg
+      .transition()
+      .select('.line')
+      .duration(0)
+      .attr('d', myLine(dataWithKey as unknown));
+  }
+
+  function dragended(this: EnterElement, _event: unknown, _d: unknown) {
+    select(this).attr('stroke', null);
+    select(this).raise().attr('fill', '#69b3a2');
+  }
+
+  let dragHandler = drag().on('start', dragstarted).on('drag', dragged).on('end', dragended);
+
+  // Add the points
+  svg
+    .append('g')
+    .selectAll('dot')
+    .data(dataWithKey)
+    .join('circle')
+    .attr('cx', (d) => customX(d[dataKeyA as unknown as number] as unknown as number))
+    .attr('cy', (d) => customY(d[dataKeyB as unknown as number] as unknown as number))
+    .attr('r', 5)
+    .attr('fill', '#69b3a2')
+    .call(dragHandler as unknown);
+
+  svg
+    .append('path')
+    .datum(dataWithKey)
+    .attr('fill', 'none')
+    .attr('stroke', '#69b3a2')
+    .attr('stroke-width', 1.5)
+    .attr('class', 'line')
+    .attr('d', myLine as unknown as string);
+});
 </script>
+
+<template>
+  <div ref="resizeRef" id="svgRef">
+    <svg ref="svgRef">
+      <g class="x-axis" />
+      <g class="y-axis" />
+    </svg>
+  </div>
+</template>
--- src/pages/StartUp/WUE.vue
+++ src/pages/StartUp/WUE.vue
@@ -1,31 +1,26 @@
 <template>
   <div>
-   <!--  <h4 class="q-mt-md q-mb-md">DTC Status</h4>
+    <!--  <h4 class="q-mt-md q-mb-md">DTC Status</h4>
     <h6 class="q-mt-md q-mb-lg">you can view and remove dtc/fault codes from here</h6> -->
-    <LineChart />
+    <LineChart dataKeyA="test" dataKeyB="test2" :data="mockData" />
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { ref } from 'vue';
-
-import { storeKey } from '../../store';
-import { useStore } from 'vuex';
+<script setup lang="ts">
+/* import { storeKey } from '../../store';
+import { useStore } from 'vuex'; */
 import LineChart from 'src/components/LineChart.vue';
 
-export default defineComponent({
-  name: 'StartUpWUE',
+/* const $store = useStore(storeKey);
+ *//*  const mockData = [
+  { test: 23, test2: 34 },
+  { test: 40, test2: 45 },
+  { test: 60, test2: 40 },
+];
+ */
 
-  components: { LineChart },
 
-  setup() {
-    const $store = useStore(storeKey);
-
-    return {
-      store: $store.state.DtcCodes,
-    };
-  },
-  methods: {},
-});
+const mockData = {};
+/* const store = $store.state.DtcCodes;
+ */
 </script>
